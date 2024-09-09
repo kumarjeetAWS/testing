9efa6db37c6edadaa8f458ae841457b0d7c45eea
kumarjeetAWS <daskumr@amazon.com>
2024-09-09 14:29:32 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
-import SECRET_VALUE from './secrets';
+import SECRET_VALUE from './secrets.js';
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
