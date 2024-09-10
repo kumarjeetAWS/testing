ebfd45c5a0aba0adf1d381a01af363ab06a32906
kumarjeetAWS <daskumr@amazon.com>
2024-09-10 21:01:32 +0530

    Update fetch-secrets.js

--- fetch-secrets.js
+++ fetch-secrets.js
@@ -1,1 +1,19 @@
+const AWS = require('aws-sdk');
+/**
 
+const fs = require('fs');
+const path = require('path');
+*/
+const secretsManager = new AWS.SecretsManager({region: 'us-east-1'});
+
+async function getSecretsValue(secretName){
+  try{
+    const data = await secretsManager.getSecretValue({SecretId: secretName}).promise();
+    console.log("data=>",data);
+  }catch(error){
+    console.error("error=>",error);
+  }
+}
+
+const secret = await getSecretsValue("my-amplify-secret");
+console.log("secret=>",secret);
