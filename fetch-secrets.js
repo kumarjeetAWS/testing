77bd7284b5d7c0b3e2e821e49f349fc8015dcd85
kumarjeetAWS <daskumr@amazon.com>
2024-09-10 22:09:07 +0530

    Update fetch-secrets.js

--- fetch-secrets.js
+++ fetch-secrets.js
@@ -5,15 +5,37 @@ const fs = require('fs');
 const path = require('path');
 */
 const secretsManager = new AWS.SecretsManager({region: 'us-east-1'});
+const amplify = new AWS.Amplify({region: 'us-east-1'});
 
 async function getSecretsValue(secretName){
   try{
     const data = await secretsManager.getSecretValue({SecretId: secretName}).promise();
     console.log("data=>",data);
+    if('SecretString' in data){
+      console.log("SecretString=>", JSON.parse(data.SecretString));
+      const resp = await updateAmplifySecrets("dzyja44gtci76","main",JSON.parse(data.SecretString));
+      console.log("resp=>",resp);
+    }
   }catch(error){
     console.error("error=>",error);
   }
 }
 
 const secret = await getSecretsValue("my-amplify-secret");
-console.log("secret=>",secret);
+
+async function updateAmplifySecrets(appId,envName,secretValues){
+  try{
+    const updatedVars = { ...secretValues };
+    console.log("updatedVars=>",updatedVars);
+    const params = {
+      appId: appId,
+      branchName: envName,
+      enviromentVariables: updatedVars,
+    }
+    const updateData = await amplify.updateBranch(params).promise();
+    console.log("updateData=>",updateData);
+    return updateData;
+  }catch(error){
+    console.error("error=>",error);
+  }
+}
