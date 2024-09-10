5f68716e0fd4aecf5ed0db21703ba64cf188e045
kumarjeetAWS <daskumr@amazon.com>
2024-09-10 12:37:56 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+const AWS = require ('aws-sdk');
 const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  
+  const aKeyId = "AKIA47CRWXVKPWUAGJMY";
+  const aKey = "60qBpLClA5Cxiyyk3e+PjjKpjf/GL4attOuyYpfP";
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
@@ -13,10 +15,37 @@ function App() {
   }, []);
 
   function createTodo() {
-    console.log("REACT_APP_SECRET_VALUE->",process.env.REACT_APP_SECRET_VALUE);
-    console.log("ENV->",process.env);
+    loadEnvVars();
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
+
+  const loadEnvVars = async () => {
+    const envVars:any = {};
+  
+    // Configure AWS SDK with your credentials
+    AWS.config.update({
+      region: 'us-east-1',
+      accessKeyId: aKeyId,
+      secretAccessKey: aKey,
+    });
+  
+    const secretsManager = new AWS.SecretsManager();
+    console.log("secretsManager->",secretsManager);
+    // Fetch secrets from Secrets Manager
+    const secretsData = await secretsManager.getSecretValue({ SecretId: 'your-secret-id' }).promise();
+    console.log("secretsData->",secretsData);
+    const secrets = JSON.parse(secretsData.SecretString);
+    console.log("secrets->",secrets);
+
+    // Loop through the secrets and add NUXT_ variables to envVars
+    for (const secret of Object.keys(secrets)) {
+      if (secret.startsWith('NUXT_')) {
+        envVars[secret] = secrets[secret];
+      }
+    }
+    console.log("envVars->",envVars);
+    return envVars;
+  };
   
   return (
     <main>
